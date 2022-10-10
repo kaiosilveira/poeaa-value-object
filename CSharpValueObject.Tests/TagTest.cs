972c8c2d0ab098dd300b378b1780df209a1d4953
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-10 10:22:12 +0100

    add tests

---
+++ CSharpValueObject.Tests/TagTest.cs
@@ -0,0 +1,45 @@
+using Xunit;
+using System.Collections.Generic;
+using CSharpValueObject.Example;
+
+namespace CSharpValueObject.Tests;
+
+public class TagTests
+{
+  [Fact]
+  public void TestIsNotEqualNull()
+  {
+    var tag = new Tag(text: "work");
+    Assert.False(tag.Equals(null));
+  }
+
+  [Fact]
+  public void TestIsNotEqualDifferentInstance()
+  {
+    var tag = new Tag(text: "work");
+    var person = new Person(firstName: "Kaio", lastName: "Silveira");
+    Assert.False(tag.Equals(person));
+  }
+
+  [Fact]
+  public void TestIsEqualAnotherTagWithSameText()
+  {
+    var tag1 = new Tag(text: "work");
+    var tag2 = new Tag(text: "work");
+    Assert.Equal(tag1, tag2);
+  }
+
+  [Fact]
+  public void TestIsFoundCorrectlyInsideHashSets()
+  {
+    var tag = new Tag(text: "work");
+    var hashSet = new HashSet<Tag>();
+
+    hashSet.Add(tag);
+    Assert.Single(hashSet);
+
+    var result = new Tag(text: "");
+    Assert.True(hashSet.TryGetValue(tag, out result));
+    Assert.Equal(tag, result);
+  }
+}
